9dc1f7929f4069ec46f901f8b306f0f05b3ef46c
deepak2406k <kulkarni.deepak@gmail.com>
2021-11-04 15:24:14 +0000

    war deploy

--- src/main/java/com/example/springboot/Controller.java
+++ src/main/java/com/example/springboot/Controller.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Controller{
 
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String index() {
-		return "Hello Controller!";
+		return "Default Controller !";
 	}
 
 }
